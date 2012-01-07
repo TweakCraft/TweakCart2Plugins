044b0f8adc49ae6e8201fd65f9d60990f4d3ed9e
Lennart Buit <lennartbuit@gmail.com>
2012-01-07 19:11:57 +0100

    lightweight elevators

--- Elevators/src/net/tweakcraft/tweakcart2elevators/Elevator.java
+++ Elevators/src/net/tweakcraft/tweakcart2elevators/Elevator.java
@@ -1,5 +1,10 @@
 package net.tweakcraft.tweakcart2elevators;
 
+import org.bukkit.Location;
+import org.bukkit.block.Block;
+import org.bukkit.block.BlockFace;
+import org.bukkit.block.Sign;
+import org.bukkit.entity.Minecart;
 import net.tweakcraft.tweakcart.TweakCart;
 import net.tweakcraft.tweakcart.TweakPluginManager;
 import net.tweakcraft.tweakcart.api.TweakCartEvent;
@@ -7,18 +12,15 @@ import net.tweakcraft.tweakcart.api.event.TweakVehicleCollidesWithSignEvent;
 import net.tweakcraft.tweakcart.api.plugin.AbstractSignPlugin;
 import net.tweakcraft.tweakcart.model.Direction;
 import net.tweakcraft.tweakcart.util.VehicleUtil;
-import org.bukkit.Location;
-import org.bukkit.block.Block;
-import org.bukkit.block.BlockFace;
-import org.bukkit.block.Sign;
-import org.bukkit.entity.Minecart;
+import net.tweakcraft.tweakcart2elevators.ElevatorParser.ElevateDirection;
 
-public class Elevators extends AbstractSignPlugin {
+public class Elevator extends AbstractSignPlugin {
+    
     private static final int MAX_HEIGHT = 32;
     private ElevatorParser parser = null;
 
-
-    public Elevators(TweakCart p) {
+    
+    public Elevator(TweakCart p) {
         super(p);
     }
 
@@ -33,51 +35,44 @@ public class Elevators extends AbstractSignPlugin {
     public String getPluginName() {
         return "Elevators";
     }
-
+    
     @Override
     public void onSignCollision(TweakVehicleCollidesWithSignEvent event) {
         Minecart mine = event.getMinecart();
         Sign sign = event.getSign();
         Direction heading = event.getDirection();
-        ElevatorParser.ElevateDirection ev = parser.parseSign(sign);
-
+        ElevateDirection ev = parser.parseSign(sign);
+        
         Location newLocation = null;
         switch(ev){
-            case UP:
-                newLocation = findSign(true, sign);
-                break;
-            case DOWN:
-                newLocation = findSign(false, sign);
-                break;
-            case INVALID:
-                //Error, do something, or just stand here and watch?
-                //yes stading and watching it is;
-                return;
+        case UP:
+            newLocation = findSign(true, sign);
+            break;
+        case DOWN:
+            newLocation = findSign(false, sign);
+            break;
         }
         if(newLocation != null){
-            //Een beetje verplaatsen wegens direction;
-            //TODO: iets met private
+            //Een beetje verplaatsen wegens direction, anders komt de cart in de sign te staan, nu blijft de cart dezelfde kant op rijden
             VehicleUtil.moveCart(mine, newLocation.add((double)heading.getModX(),(double)heading.getModY(),(double)heading.getModZ()));
         }
     }
 
     private Location findSign(boolean isUp, Sign s) {
         Block signBlock = s.getBlock();
-        int i = 0;
         int height = s.getY();
-        int maxIt = Math.min(isUp? 129 - height : height, MAX_HEIGHT); //Potential to break everything, and i like it that way
+        int maxIt = Math.min(isUp? 129 - height : height, MAX_HEIGHT); 
         BlockFace direction = isUp? BlockFace.UP : BlockFace.DOWN;
-
-        while(i < maxIt){
+        
+        for(int i = 0; i < maxIt; i++){
             signBlock = signBlock.getRelative(direction);
             if(signBlock instanceof Sign){
                 return signBlock.getLocation();
             }
-            i++;
         }
         return null;
     }
-
+    
 
 
 }
