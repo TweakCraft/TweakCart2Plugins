e4c656785ef6dec94d2e6e3bb959ddd549f85b03
Edoxile <koen_75_@hotmail.com>
2012-01-20 00:12:03 +0100

    Was that it? It can't be...

--- CartStorage/src/net/tweakcraft/tweakcart2cartstorage/InventoryManager.java
+++ CartStorage/src/net/tweakcraft/tweakcart2cartstorage/InventoryManager.java
@@ -13,31 +13,40 @@ public class InventoryManager {
     public static int moveContainerContents(Inventory iFrom, Inventory iTo, IntMap map) {
         ItemStack[] from = iFrom.getContents();
         ItemStack[] to = iTo.getContents();
-        for (int i = 0; i < from.length; i++) {
+        fromLoop:for (int i = 0; i < from.length; i++) {
             ItemStack fStack = from[i];
             if (fStack == null || !fStack.getEnchantments().isEmpty()) {
                 continue;
             }
-            for (int j = 0; j < to.length; j++) {
+            toLoop:for (int j = 0; j < to.length; j++) {
                 ItemStack tStack = to[i];
                 if (tStack == null) {
-                    tStack = fStack;
-                    fStack = null;
+                    to[i] = fStack;
+                    from[i] = null;
+                    continue fromLoop;
                 } else if (tStack.getAmount() == 64) {
                     continue;
                 } else if (fStack.getTypeId() == tStack.getTypeId() && fStack.getDurability() == tStack.getDurability() && tStack.getEnchantments().isEmpty()) {
                     //And now the magic begins
                     int total = fStack.getAmount() + tStack.getAmount();
-                    if (total >= 64) {
-                        //TODO: use this value to substract from IntMap.
-                        int removed = 64 - tStack.getAmount();
-                        map.setInt(tStack.getType(), (byte) tStack.getDurability(), map.getInt(tStack.getType(), (byte) tStack.getDurability()) - removed);
+                    if (total > 64) {
+                        map.setInt(tStack.getType(), (byte) tStack.getDurability(), map.getInt(tStack.getType(), (byte) tStack.getDurability()) - (64 - tStack.getAmount()));
+                        tStack.setAmount(64);
+                        fStack.setAmount(total-64);
+                    } else {
+                        map.setInt(tStack.getType(), (byte) tStack.getDurability(), map.getInt(tStack.getType(), (byte) tStack.getDurability()) - fStack.getAmount());
+                        tStack.setAmount(total);
+                        from[i] = null;
+                        continue fromLoop;
                     }
+                } else {
+                    continue;
                 }
                 to[i] = tStack;
             }
             from[i] = fStack;
         }
+        //For now, just return 0. Check are to be built in after this is properly tested.
         return 0;
     }
 }
