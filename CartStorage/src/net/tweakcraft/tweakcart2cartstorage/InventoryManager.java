67324df17848a5ba4676413024b3f7411c635ece
Edoxile <koen_75_@hotmail.com>
2012-01-20 00:31:41 +0100

    Now it should work with IntMaps

--- CartStorage/src/net/tweakcraft/tweakcart2cartstorage/InventoryManager.java
+++ CartStorage/src/net/tweakcraft/tweakcart2cartstorage/InventoryManager.java
@@ -13,12 +13,15 @@ public class InventoryManager {
     public static int moveContainerContents(Inventory iFrom, Inventory iTo, IntMap map) {
         ItemStack[] from = iFrom.getContents();
         ItemStack[] to = iTo.getContents();
-        fromLoop:for (int i = 0; i < from.length; i++) {
+        fromLoop:
+        for (int i = 0; i < from.length; i++) {
             ItemStack fStack = from[i];
-            if (fStack == null || !fStack.getEnchantments().isEmpty()) {
+            int maxAmountToMove = map.getInt(fStack.getType(), (byte) fStack.getDurability());
+            if (fStack == null || !fStack.getEnchantments().isEmpty() || maxAmountToMove == 0) {
                 continue;
             }
-            toLoop:for (int j = 0; j < to.length; j++) {
+            toLoop:
+            for (int j = 0; j < to.length; j++) {
                 ItemStack tStack = to[i];
                 if (tStack == null) {
                     to[i] = fStack;
@@ -28,16 +31,38 @@ public class InventoryManager {
                     continue;
                 } else if (fStack.getTypeId() == tStack.getTypeId() && fStack.getDurability() == tStack.getDurability() && tStack.getEnchantments().isEmpty()) {
                     //And now the magic begins
-                    int total = fStack.getAmount() + tStack.getAmount();
-                    if (total > 64) {
-                        map.setInt(tStack.getType(), (byte) tStack.getDurability(), map.getInt(tStack.getType(), (byte) tStack.getDurability()) - (64 - tStack.getAmount()));
-                        tStack.setAmount(64);
-                        fStack.setAmount(total-64);
+                    //First check if the stackAmount is smaller then the max amount to move
+                    if (fStack.getAmount() <= maxAmountToMove) {
+                        int total = fStack.getAmount() + tStack.getAmount();
+                        if (total > 64) {
+                            map.setInt(tStack.getType(), (byte) tStack.getDurability(), map.getInt(tStack.getType(), (byte) tStack.getDurability()) - (64 - tStack.getAmount()));
+                            tStack.setAmount(64);
+                            fStack.setAmount(total - 64);
+                        } else {
+                            map.setInt(tStack.getType(), (byte) tStack.getDurability(), map.getInt(tStack.getType(), (byte) tStack.getDurability()) - fStack.getAmount());
+                            tStack.setAmount(total);
+                            from[i] = null;
+                            continue fromLoop;
+                        }
                     } else {
-                        map.setInt(tStack.getType(), (byte) tStack.getDurability(), map.getInt(tStack.getType(), (byte) tStack.getDurability()) - fStack.getAmount());
-                        tStack.setAmount(total);
-                        from[i] = null;
-                        continue fromLoop;
+                        //Otherwise, run some other code
+                        int total = maxAmountToMove + tStack.getAmount();
+                        int stableAmount = fStack.getAmount() - maxAmountToMove;
+                        if (total > 64) {
+                            map.setInt(tStack.getType(), (byte) tStack.getDurability(), map.getInt(tStack.getType(), (byte) tStack.getDurability()) - (64 - tStack.getAmount()));
+                            maxAmountToMove -= 64 - tStack.getAmount();
+                            tStack.setAmount(64);
+                            fStack.setAmount(total - 64 + stableAmount);
+                        } else {
+                            map.setInt(tStack.getType(), (byte) tStack.getDurability(), map.getInt(tStack.getType(), (byte) tStack.getDurability()) - maxAmountToMove);
+                            tStack.setAmount(total);
+                            if (stableAmount > 0) {
+                                from[i].setAmount(stableAmount);
+                            } else {
+                                from[i] = null;
+                            }
+                            continue fromLoop;
+                        }
                     }
                 } else {
                     continue;
