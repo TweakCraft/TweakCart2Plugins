128cbdd5a022cbb2e592caed9dfc6619def0dfa8
niaerts <nick.aerts@ugent.be>
2015-03-30 21:21:40 +0200

    Removed some unwantend println's

--- CartRouting/src/net/tweakcraft/tweakcart/cartrouting/HelperClass.java
+++ CartRouting/src/net/tweakcraft/tweakcart/cartrouting/HelperClass.java
@@ -12,20 +12,15 @@ public class HelperClass {
 
     public static Sign findSign(Block block, String firstLine){
         int[][] locations = { {1,0}, {0,1}, {-1,0}, {0,-1} }; /* A diff in every direction from the origin */
-        System.out.println("FindSign!");
-        System.out.println(block.getType().name());
 
-        for(int[] diff : locations) {
+        for(int[] diff : locations)
             if (BlockUtil.isSign(block.getRelative(diff[0], 0, diff[1]))){
                 Sign sign = (Sign) block.getRelative(diff[0], 0, diff[1]).getState();
                 if(sign.getLine(0).equalsIgnoreCase(ActionType.GETDEST.getText()))
                     return sign;
                 else return null;
-            } else
-                System.out.println(block.getRelative(diff[0], 0, diff[1]).getType().name());
-        }
-        System.out.println("End FindSign!");
+            }
+
         return null;
     }
-
 }
